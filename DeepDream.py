ad9383f2b4a9b429cf6e0b2fd28c7b2d18128d4b
deepansh11 <34342846+deepansh11@users.noreply.github.com>
2019-08-06 17:51:57 +0530

    Generates Images using Google's Inception Model

---
+++ DeepDream.py
@@ -0,0 +1,130 @@
+import numpy as np
+from functools import partial
+import PIL.Image
+import tensorflow as tf
+import matplotlib.pyplot as plt
+import urllib.request
+import os
+import zipfile
+
+def main():
+    #Step 1 - download google's pre-trained neural network
+    data_dir = '../data/'
+    output_dir = '../output/'
+    model_fn = '/home/lenovo/Desktop/python/tensorflow_inception_graph.pb'
+    output_name = 'DeepDreamAI'
+
+    # start with a gray image with a little noise
+    img_noise = np.random.uniform(size=(224,224,3)) + 100.0
+
+    #Step 2 - Creating Tensorflow session and loading the model
+    graph = tf.Graph()
+    sess = tf.InteractiveSession(graph=graph)
+    with tf.gfile.FastGFile(os.path.join(data_dir, model_fn), 'rb') as f:
+        graph_def = tf.GraphDef()
+        graph_def.ParseFromString(f.read())
+    t_input = tf.placeholder(np.float32, name='input') # define the input tensor
+    imagenet_mean = 117.0
+    t_preprocessed = tf.expand_dims(t_input-imagenet_mean, 0)
+    tf.import_graph_def(graph_def, {'input':t_preprocessed})
+
+    layers = [op.name for op in graph.get_operations() if op.type=='Conv2D' and 'import/' in op.name]
+    feature_nums = [int(graph.get_tensor_by_name(name+':0').get_shape()[-1]) for name in layers]
+
+    print('Number of layers', len(layers))
+    print('Total number of feature channels:', sum(feature_nums))
+
+    # Helper functions for TF Graph visualization
+    #pylint: disable=unused-variable
+
+    def show_array(arr):
+        arr = np.uint8(np.clip(arr, 0, 1)*255)
+        plt.imshow(arr)
+        plt.show()
+
+    def save_array(arr):
+        arr = np.uint8(np.clip(arr, 0, 1)*255)
+        with open(output_dir + output_name + '.jpg', 'wb') as file:
+            PIL.Image.fromarray(arr).save(file, 'jpeg')
+
+    def T(layer):
+        '''Helper for getting layer output tensor'''
+        return graph.get_tensor_by_name("import/%s:0"%layer)
+
+    def tffunc(*argtypes):
+        '''Helper that transforms TF-graph generating function into a regular one.
+        See "resize" function below.
+        '''
+        placeholders = list(map(tf.placeholder, argtypes))
+        def wrap(f):
+            out = f(*placeholders)
+            def wrapper(*args, **kw):
+                return out.eval(dict(zip(placeholders, args)), session=kw.get('session'))
+            return wrapper
+        return wrap
+
+    def resize(img, size):
+        img = tf.expand_dims(img, 0)
+        return tf.image.resize_bilinear(img, size)[0,:,:,:]
+    resize = tffunc(np.float32, np.int32)(resize)
+
+    def calc_grad_tiled(img, t_grad, tile_size=512):
+        '''Compute the value of tensor t_grad over the image in a tiled way.
+        Random shifts are applied to the image to blur tile boundaries over
+        multiple iterations.'''
+        sz = tile_size
+        h, w = img.shape[:2]
+        sx, sy = np.random.randint(sz, size=2)
+        img_shift = np.roll(np.roll(img, sx, 1), sy, 0)
+        grad = np.zeros_like(img)
+        for y in range(0, max(h-sz//2, sz),sz):
+            for x in range(0, max(w-sz//2, sz),sz):
+                sub = img_shift[y:y+sz,x:x+sz]
+                g = sess.run(t_grad, {t_input:sub})
+                grad[y:y+sz,x:x+sz] = g
+        return np.roll(np.roll(grad, -sx, 1), -sy, 0)
+
+    def render_deepdream(t_obj, img0=img_noise,
+                         iter_n=10, step=1.5, octave_n=4, octave_scale=1.4):
+        t_score = tf.reduce_mean(t_obj) # defining the optimization objective
+        t_grad = tf.gradients(t_score, t_input)[0] # behold the power of automatic differentiation!
+
+        # split the image into a number of octaves
+        img = img0
+        octaves = []
+        for _ in range(octave_n-1):
+            hw = img.shape[:2]
+            lo = resize(img, np.int32(np.float32(hw)/octave_scale))
+            hi = img-resize(lo, hw)
+            img = lo
+            octaves.append(hi)
+
+        # generate details octave by octave
+        for octave in range(octave_n):
+            if octave>0:
+                hi = octaves[-octave]
+                img = resize(img, hi.shape[:2])+hi
+            for _ in range(iter_n):
+                g = calc_grad_tiled(img, t_grad)
+                img += g*(step / (np.abs(g).mean()+1e-7))
+
+            #this will usually be like 3 or 4 octaves
+            #Step 5 output deep dream image via matplotlib
+            show_array(img/255.0)
+
+
+
+   	#Step 3 - Pick a layer to enhance our image
+    layer = 'mixed4d_3x3_bottleneck_pre_relu'
+    channel = 139 # picking some feature channel to visualize
+
+    #open image
+    img0 = PIL.Image.open('scene.jpg')
+    img0 = np.float32(img0)
+
+    #Step 4 - Apply gradient ascent to that layer
+    render_deepdream(tf.square(T('mixed4c')), img0)
+
+
+if __name__ == '__main__':
+    main()
